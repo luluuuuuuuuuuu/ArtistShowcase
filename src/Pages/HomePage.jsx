907ac1111ff87923e16c6f9c3475402507271b05
Lucia <luludoesnfts@gmail.com>
2023-06-20 10:03:39 +0200

    "console log to check if data is fetched"

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,16 @@
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
 import UpcomingConcerts from '../Components/UpcomingConcerts'
+import TourMap from '../Components/TourMap'
+
 
 export default function HomePage() {
     return (
         <div>
             <Navbar />
             <Hero />
-            <UpcomingConcerts />          
+            <UpcomingConcerts /> 
+            <TourMap />         
         </div>
     )
 }
